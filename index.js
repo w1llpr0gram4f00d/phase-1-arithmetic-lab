f93a915f1d62d762583aebad278b0c5bf40f6965
Marc Cavazos <marcavazos19@icloud.com>
2022-01-10 17:53:35 -0500

    commit tree ecf2178cb318083b90de88bbe67538c0243aa6c7

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+const num1 = 2;
+const num2 = 31;
+let multiply = num1 * num2;
+
+let random = (Math.floor((Math.random() * 10) + 1));
+
+const num3 = 17;
+const num4 = 13;
+let mod = num3 % num4;
+
+const max = Math.max(1, 2, 20, 15);
